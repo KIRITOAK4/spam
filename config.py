5c437266e24587a7eb34a400892271933bcc6ba7
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2024-02-16 12:35:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,3 @@ import os, re
 API_ID = os.environ.get("API_ID", )
 API_HASH = os.environ.get("API_HASH", "")
 SESSION_STRING = os.environ.get("SESSION_STRING", "")
-#API_ID = os.environ.get("API_ID", 14712540)
-#API_HASH = os.environ.get("API_HASH", "e61b996dc037d969a4f8cf6411bb6165")
-#SESSION_STRING = os.environ.get("SESSION_STRING", "BQDgftwAwtNx7rFmWHT9jIpbD-N-j95sBQTQl63jSIW7x_ihPU-kxXwv1frmljeRZbyOFq0dmcCSe9Cq7fOFqVMA61JZnh0TOOZcqlLOdFNkMNPAYisMcoWYD6PVqJy6rqPsOqHd9ff3ib60WCcdJoPeKWLNJJMgox_4YXtVBwW6dt8bKbfemMKcns6BADkInojvvQGFGm9nCbf3S15nklH8dmmNllene6T-x4j8BQW_5puTv38M0cM5jIbclsdRTKuQAaofdieCjgLmFCK7JJawrihxvzViZRDO-TlEkftUw8T4Hzce7eQfRBJN3PcOtHAJY4XwbbuFFq_fCw_pcwZBkC2lHAAAAAB3wEBrAA")
