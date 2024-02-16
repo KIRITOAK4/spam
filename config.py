89e095f13b8810b7b6effbea97b93fffaa71fe8c
ㅤ <127006482+KIRITOAK4@users.noreply.github.com>
2024-02-16 12:40:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os, re
 API_ID = os.environ.get("API_ID", )
 API_HASH = os.environ.get("API_HASH", "")
 SESSION_STRING = os.environ.get("SESSION_STRING", "")
+OWNER_ID = os.environ.get("OWNER_ID", )
